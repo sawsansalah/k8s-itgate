cade22d6a44969d8287030e0bf9d1ff6f8836237
sawsansalah <sawsan.salaheldin@gmail.com>
2025-05-05 10:14:22 +0300

    Update certificate.sh

--- ingress/certificate.sh
+++ ingress/certificate.sh
@@ -1,1 +1,7 @@
 openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -keyout tls.key -out tls.crt -subj "/CN=login.example.com/O=example.com"
+
+kubectl create secret tls my-tls-secret \
+  --cert=tls.crt \
+  --key=tls.key \
+  -n default
+
